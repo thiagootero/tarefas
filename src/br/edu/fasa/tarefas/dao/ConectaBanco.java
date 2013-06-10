d022461207e5eff5966a4f2be38400d9378442ea
thiago <thiago@thiago-PC>
2013-06-10 13:35:27 -0300

    ConectarBanco

---
+++ src/br/edu/fasa/tarefas/dao/ConectaBanco.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fasa.tarefas.dao;
+
+/**
+ *
+ * @author thiago
+ */
+public class ConectaBanco {
+    private int a;
+}
